863eb6d4a0dae3800c9137d309a792f6d066ef69
juan94v <juanm9413@hotmail.com>
2023-08-30 07:47:28 -0600

    feat: Import views (i could not use @ as src), Adds nav

--- src/App.vue
+++ src/App.vue
@@ -3,15 +3,11 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <router-view />
 </template>
 
 <style scoped>
